99504ef2f2643ad7806cc69a7543b22068599266
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-08 14:14:32 +0800

    Rename vite.config.ts to vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      port: 3000,
+    },
+  };
+});
